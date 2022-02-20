f5d0201f57b3491dda77ec1f758c57dd8c5cb62d
Jon B Mårtensson <53905247+Jon-b-m@users.noreply.github.com>
2022-02-20 21:51:41 +0100

    Format

--- Chris_with_calculated_TDD.js
+++ Chris_with_calculated_TDD.js
@@ -1,6 +1,6 @@
 function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock, pumphistory) {
  	
-  // This middleware only works if you have added pumphistory to middleware in FreeAPS X code (my pumphistory branch).
+	// This middleware only works if you have added pumphistory to middleware in FreeAPS X code (my pumphistory branch).
 	const BG = glucose[0].glucose;
 	// Change to false to turn off Chris Wilson's formula
 	var chrisFormula = true;
@@ -12,14 +12,14 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
 	const currentMinTarget = profile.min_bg;
 	var exerciseSetting = false;
 	var log = "";
-  var logTDD = "";
-  var current = 0;
-  // If you have not set this to 0.05 in FAX settings (Omnipod), this will be set to 0.1 in code.
-  var minimalDose = profile.bolus_increment;
-  var insulin = 0.00;
-  var incrementsRaw = 0.00;
-  var incrementsRounded = 0.00;
-  var quota = 0;
+	var logTDD = "";
+  	var current = 0;
+  	// If you have not set this to 0.05 in FAX settings (Omnipod), this will be set to 0.1 in code.
+  	var minimalDose = profile.bolus_increment;
+  	var insulin = 0.00;
+  	var incrementsRaw = 0.00;
+  	var incrementsRounded = 0.00;
+  	var quota = 0;
 	
 	if (profile.high_temptarget_raises_sensitivity == true || profile.exercise_mode == true) {
 		exerciseSetting = true;
@@ -33,55 +33,55 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
 		log = "Chris' formula off due to a high temp target/exercising. Current min target: " + currentMinTarget;
 	} 
 
-  // Calculate TDD --------------------------------------
+	// Calculate TDD --------------------------------------
     
-  //Bolus:
-  for (let i = 0; i < pumphistory.length; i++) {
-    if (pumphistory[i]._type == "Bolus") {
-      // Bolus delivered
-      TDD += pumphistory[i].amount;
-    }  
-  }
+  	//Bolus:
+  	for (let i = 0; i < pumphistory.length; i++) {
+    		if (pumphistory[i]._type == "Bolus") {
+      			// Bolus delivered
+      			TDD += pumphistory[i].amount;
+    		}  
+  	}
 
-  // Temp basals:
-  for (let j = 1; j < pumphistory.length; j++) {
-    if (pumphistory[j]._type == "TempBasal" && pumphistory[j].rate > 0) {
-      current = j;
-      quota = pumphistory[j].rate;
-      var duration = pumphistory[j-1]['duration (min)'] / 60;
-      var origDur = duration;
-      var pastTime = new Date(pumphistory[j].timestamp);
+	// Temp basals:
+  	for (let j = 1; j < pumphistory.length; j++) {
+    		if (pumphistory[j]._type == "TempBasal" && pumphistory[j].rate > 0) {
+      			current = j;
+      			quota = pumphistory[j].rate;
+     			 var duration = pumphistory[j-1]['duration (min)'] / 60;
+      			var origDur = duration;
+      			var pastTime = new Date(pumphistory[j].timestamp);
                 
-      do {
-        --j;
-      }
-      while (pumphistory[j]._type !== "TempBasal");
+      			do {
+        			--j;
+      			}
+      			while (pumphistory[j]._type !== "TempBasal");
                 
-      var morePresentTime = new Date(pumphistory[j].timestamp);
-      var diff = (morePresentTime - pastTime) / 6e4;
-      if (origDur > diff) {
-        duration = diff;
-      }
-      insulin = quota * duration;
+      			var morePresentTime = new Date(pumphistory[j].timestamp);
+      			var diff = (morePresentTime - pastTime) / 6e4;
+      			if (origDur > diff) {
+        			duration = diff;
+      			}
+      			insulin = quota * duration;
             
-      // Account for smallest possible pump dosage
-      if (minimalDose != 0.05) {
-        minimalDose = 0.1;
-      }
-      incrementsRaw = insulin / minimalDose;
-      if (incrementsRaw >= 1) {
-        incrementsRounded = Math.floor(incrementsRaw);
-        insulin = incrementsRounded * minimalDose;
-      } else { insulin = 0}
+      			// Account for smallest possible pump dosage
+      			if (minimalDose != 0.05) {
+        			minimalDose = 0.1;
+      			}
+      			incrementsRaw = insulin / minimalDose;
+      			if (incrementsRaw >= 1) {
+        			incrementsRounded = Math.floor(incrementsRaw);
+        			insulin = incrementsRounded * minimalDose;
+      			} else { insulin = 0}
             
-      // Add temp basal delivered to TDD
-      TDD += insulin;
-      j = current;
-    }
-  }
-  logTDD = ". TDD past 24h is: " + TDD.toPrecision(3) + " U";
-  // ---------------------------------------------------
-  // Chris' formula:
+      			// Add temp basal delivered to TDD
+      			TDD += insulin;
+      			j = current;
+    		}
+  	}
+  	logTDD = ". TDD past 24h is: " + TDD.toPrecision(3) + " U";
+  	// ---------------------------------------------------
+  	// Chris' formula:
   
 	if (chrisFormula == true && TDD > 0) {
 		var newRatio = profile.sens / (277700 / (adjustmentFactor  * TDD * BG));
@@ -97,7 +97,7 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
 		  }
 
 		  // Set the new ratio
-    	autosens.ratio = newRatio;
+    		autosens.ratio = newRatio;
 	
 		  return log + logTDD;
 	} else { return "Chris' formula is disabled." }	
