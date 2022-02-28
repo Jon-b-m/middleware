81a35985e8ae1e0a0a2ee36c1382c57e6d9900ca
Jon B Mårtensson <53905247+Jon-b-m@users.noreply.github.com>
2022-02-28 19:05:11 +0100

    Update c_formula_with_conditions.js

--- c_formula_with_conditions.js
+++ c_formula_with_conditions.js
@@ -1,7 +1,7 @@
 function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoir, clock) {
  	
 	const BG = glucose[0].glucose;
-	// To turn off change to false
+	// To turn off, change to false
 	var chrisFormula = true;
 	// Change to your own TDD
 	const TDD = 58;
@@ -22,12 +22,13 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
 	if (currentMinTarget >= 118 && exerciseSetting == true) {
 		// profile.use_autoisf = false;
 		chrisFormula = false;
-		log = "Chris' formula off due to a high temp target/exercising. Current min target: " + currentMinTarget;
+		log = "Chris' formula off due to a high temp target while exercising. Current min target: " + currentMinTarget;
 	} 
 
-	if (chrisFormula == true && TDD > 0) {
-		var newRatio = profile.sens / (277700 / (adjustmentFactor  * TDD * BG));
-		log = "New ratio using Chris' formula is " + newRatio.toPrecision(4) + " with ISF: " + (profile.sens / newRatio).toPrecision(4) + ". TDD past 24h is: " + TDD;
+	// Chris' formula with added adjustmentFactor for tuning:
+    	if (chrisFormula == true && TDD > 0) {
+        	var newRatio = profile.sens / (277700 / (adjustmentFactor  * TDD * BG));
+        	log = "New ratio using Chris' formula is " + newRatio.toPrecision(3) + " with ISF: " + (profile.sens / newRatio).toPrecision(3) + " (" + ((profile.sens / newRatio) * 0.0555).toPrecision(3) + " mmol/l/U)";
 
 		// Respect autosens.max and autosens.min limits
 		if (newRatio > maxLimitChris) {
@@ -42,5 +43,5 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
     		autosens.ratio = newRatio;
 	
 		return log;
-	} else { return "Chris' formula is disabled." }	
+	} else { return "Chris' formula is off." }	
 }
