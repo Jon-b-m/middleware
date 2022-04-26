6c4c9174967196447f0cbe9e0f8f52697ca734c1
Jon B Mårtensson <53905247+Jon-b-m@users.noreply.github.com>
2022-04-26 12:04:57 +0200

    Update comments

--- Dynamic_ISF_and_CR_and_TDD.js
+++ Dynamic_ISF_and_CR_and_TDD.js
@@ -2,7 +2,6 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
      
     // This middleware will work with my dyn_ISF_and_CR branch and my bdb branch).
     const BG = glucose[0].glucose;
-    // Change to false to turn off Chris Wilson's formula
     var chrisFormula = preferences.enableChris;
     var useDynamicCR = preferences.enableDynamicCR;
     const minLimitChris = profile.autosens_min;
