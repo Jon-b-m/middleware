bd379b59bd40eaeee572a59d6a7d182ff9ab8514
Jon B Mårtensson <53905247+Jon-b-m@users.noreply.github.com>
2022-04-29 15:31:10 +0200

    Update DISF_with_TDD_and_preferences.js

--- DISF_with_TDD_and_preferences.js
+++ DISF_with_TDD_and_preferences.js
@@ -193,14 +193,8 @@ function middleware(iob, currenttemp, glucose, profile, autosens, meal, reservoi
         exerciseSetting = true;
     }
     
-    // Turns off Auto-ISF when using Dynamic ISF.
-    if (profile.use_autoisf == true && chrisFormula == true) {
-        profile.use_autoisf = false;
-    }
-    
-    // Turn off Chris' formula (and AutoISF) when using a temp target >= 118 (6.5 mol/l) and if an exercise setting is enabled.
+    // Turn off Chris' formula when using a temp target >= 118 (6.5 mol/l) and if an exercise setting is enabled.
     if (currentMinTarget >= 118 && exerciseSetting == true) {
-        profile.use_autoisf = false;
         chrisFormula = false;
         log = "Dynamic ISF temporarily off due to a high temp target/exercising. Current min target: " + currentMinTarget;
     }
